2d504197048321ec360b4008a61a6ae206cb942a
crene <crenee72@hotmail.com>
2024-02-05 16:22:22 -0700

    Revert "Update main-view.jsx"
    This reverts commit a4dc029f3511f325f0aff0b7d0511dbec0fecb14.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,30 +1,16 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-import { LoginView } from "../login-view/login-view";
-import { SignupView } from "../signup-view/signup-view";
 
 //Exports the created MainView component
 export const MainView = () => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    const storedToken = localStorage.getItem("token");
-    const [user, setUser] = useState(storedUser ? storedUser : null);
-    const [token, setToken] = useState(storedToken ? storedToken : null);
 
     //Use useState to delare a state variable that is called movies and passs the initial state (an empty array) as an argument to the useState()
     const [movies, setMovies] = useState([]);
 
     //Fetch data from API and using setMovies, populate the movies state. Do this with the fetched movies array from myFlix API
     useEffect(() => {
-        if (!token) {
-            return;
-        }
-
-        fetch('https://renee-myflix-api-2507fb668e0f.herokuapp.com/movies', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
+        fetch('https://renee-myflix-api-2507fb668e0f.herokuapp.com/movies')
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
@@ -44,22 +30,7 @@ export const MainView = () => {
                 });
                 setMovies(moviesFromApi);
             });
-    }, [token]);
-
-    if (!user) {
-        return (
-            <>
-                <LoginView
-                    onLoggedIn={(user, token) => {
-                        setUser(user);
-                        setToken(token);
-                    }}
-                />
-                or
-                <SignupView />
-            </>
-        );
-    }
+    }, []);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
@@ -75,12 +46,6 @@ export const MainView = () => {
 
     return (
         <div>
-            <button onClick={() => {
-                setUser(null);
-                setToken(null);
-                localStorage.clear();
-            }}>Logout</button>
-
             {movies.map((movie) => (
                 <MovieCard
                     key={movie.id}
