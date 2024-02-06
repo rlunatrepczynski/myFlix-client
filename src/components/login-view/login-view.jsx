6fe6d31370c44b04fa43bf2d623992751c0e98b9
crene <crenee72@hotmail.com>
2024-02-05 21:46:51 -0700

    Update login-view.jsx
    updated login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
-const LoginView =({ onLoggedIn })=>{
-    const [username, setUsername]= useState("");
+const LoginView = ({ onLoggedIn }) => {
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     const handleSubmit = (event) => {
@@ -14,38 +14,38 @@ const LoginView =({ onLoggedIn })=>{
 
         };
 
-        fetch("https://renee-myflix-api-2507fb668e0f.herokuapp.com/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(data)
+        fetch("https://renee-myflix-api-2507fb668e0f.herokuapp.com/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
         }).then((response) => response.json())
-        .then((data) => {
-          console.log("Login response: ", data);
-          if (data.user) {
-            localStorage.setItem("user", JSON.stringify(data.user));
-            localStorage.setItem("token", data.token);
-            onLoggedIn(data.user, data.token);
-          } else {
-            alert("No such user");
-          }
-        })
-        .catch((e) => {
-          alert("Something went wrong");
-        });
-    } 
-    return(
+            .then((data) => {
+                console.log("Login response: ", data);
+                if (data.user) {
+                    localStorage.setItem("user", JSON.stringify(data.user));
+                    localStorage.setItem("token", data.token);
+                    onLoggedIn(data.user, data.token);
+                } else {
+                    alert("No such user");
+                }
+            })
+            .catch((e) => {
+                alert("Something went wrong");
+            });
+    }
+    return (
         <form onSubmit={handleSubmit}>
             <label>
-                Username: <input type="text" value={username} onChange={(e)=>{setUsername(e.target.value)}} required minLength={6}/>
+                Username: <input type="text" value={username} onChange={(e) => { setUsername(e.target.value) }} required minLength={6} />
             </label>
             <label >
-                Password: <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} required />
+                Password: <input type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} required />
             </label>
             <button type="submit">Submit</button>
         </form>
     );
 };
 
-export {LoginView};
+export { LoginView };
