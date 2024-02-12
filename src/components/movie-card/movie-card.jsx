47591aaae578c318c6c05353ffc132173a072210
crene <crenee72@hotmail.com>
2024-02-12 16:10:09 -0700

    Update movie-card.jsx
    Added Card and Button to movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,20 +1,34 @@
 //Import Prop Types
 import PropTypes from 'prop-types';
+import { Button, Card } from "react-bootstrap";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-        <div
-            onClick={() => {
-                onMovieClick(movie);
-            }}
-        >
-            {movie.Title}
-        </div>
+        <Card className="h-100">
+            <Card.Img variant="top" src={movie.ImageURL} />
+            <Card.Body>
+                <Card.Title>{movie.Title}</Card.Title>
+                <Card.Text>{movie.Director.Name}</Card.Text>
+                <Button onClick={() => onMovieClick(movie)} variant="link">
+                    Open
+                </Button>
+            </Card.Body>
+        </Card>
     );
 };
 //Define props constraints for MovieCard
 MovieCard.propTypes = {
     movie: PropTypes.shape({
-        Title: PropTypes.string
+        ImageURL: PropTypes.string.isRequired,
+        Title: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+        }),
+        Description: PropTypes.string.isRequired,
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+        }),
     }).isRequired,
     onMovieClick: PropTypes.func.isRequired
 };
+
