211b3f760c8797ff21193e0cb0a3aa5a9a458182
crene <crenee72@hotmail.com>
2024-02-14 02:25:57 -0700

    Revert "Update movie-card.jsx"
    This reverts commit 65c07c5c48df1eb27a6c4f5212b5b27805b045a6.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,25 +4,18 @@ import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./movie-card.scss";
 
-export const MovieCard = ({ movie, addFav, removeFav, isFavorite }) => {
+export const MovieCard = ({ movie }) => {
     return (
         <Card className="h-100">
             <Card.Img variant="top" src={movie.ImageURL} />
             <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
                 <Card.Text>{movie.Director.Name}</Card.Text>
-                <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+                <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
                     <Button variant="link">
                         Open
                     </Button>
                 </Link>
-                <div>
-                    {isFavorite ? (
-                        <Button className="my-2 me-2" onClick={() => removeFav(movie._id)}>Remove from Favorite</Button>
-                    ) : (
-                        <Button className="my-2 me-2" onClick={() => addFav(movie._id)}>Add to Favorite</Button>
-                    )}
-                </div>
             </Card.Body>
         </Card>
     );
