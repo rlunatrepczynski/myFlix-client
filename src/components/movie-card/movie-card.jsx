65c07c5c48df1eb27a6c4f5212b5b27805b045a6
crene <crenee72@hotmail.com>
2024-02-13 23:14:41 -0700

    Update movie-card.jsx
    Added favorite movies and remove movies from Users favorites.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,18 +4,25 @@ import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./movie-card.scss";
 
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, addFav, removeFav, isFavorite }) => {
     return (
         <Card className="h-100">
             <Card.Img variant="top" src={movie.ImageURL} />
             <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
                 <Card.Text>{movie.Director.Name}</Card.Text>
-                <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+                <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
                     <Button variant="link">
                         Open
                     </Button>
                 </Link>
+                <div>
+                    {isFavorite ? (
+                        <Button className="my-2 me-2" onClick={() => removeFav(movie._id)}>Remove from Favorite</Button>
+                    ) : (
+                        <Button className="my-2 me-2" onClick={() => addFav(movie._id)}>Add to Favorite</Button>
+                    )}
+                </div>
             </Card.Body>
         </Card>
     );
