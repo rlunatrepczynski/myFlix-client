61508fafca02465678f4e797df5dab057e5b9aa9
crene <crenee72@hotmail.com>
2024-02-26 22:55:39 -0700

    Update navigation-bar.jsx
    add image to navbar

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,12 +1,21 @@
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./navigation-bar.scss";
+import movieLogo from '../../img/movie_logo3.jpg';
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
                 <Navbar.Brand as={Link} to="/">
-                    Movies App
+                    <img
+                        src={movieLogo}
+                        width="70"
+                        height="70"
+                        className="d-inline-block align-top logo"
+                        alt="Movie Logo"
+                        onError={(e) => console.error("Error loading image:", e)}
+                    />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -19,10 +28,10 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                             </>
                         ) : (
                             <>
-                                <Nav.Link as={Link} to="/signup">
+                                <Nav.Link as={Link} to="/signup" className="signup-link">
                                     Sign Up
                                 </Nav.Link>
-                                <Nav.Link as={Link} to="/login">
+                                <Nav.Link as={Link} to="/login" className="login-link">
                                     Login
                                 </Nav.Link>
                             </>
